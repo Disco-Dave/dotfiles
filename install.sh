4945aa02f7ab5f6a7082b3a8a51c07851874eac7
David Burkett <db.discodave@gmail.com>
2019-12-03 20:53:56 -0500

    Utilize tee

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@
 
 # Ensure that the XDG env variables are set
 mkdir -p $HOME/.config $HOME/.cache $HOME/.local/share
-sudo bash -c "echo XDG_CONFIG_HOME=\$HOME/.config >> /etc/environment"
-sudo bash -c "echo XDG_CACHE_HOME=\$HOME/.cache >> /etc/environment"
-sudo bash -c "echo XDG_DATA_HOME=\$HOME/.local/share >> /etc/environment"
+echo XDG_CONFIG_HOME=\$HOME/.config | sudo tee -a /etc/environment > /dev/null
+echo XDG_CACHE_HOME=\$HOME/.cache | sudo tee -a /etc/environment > /dev/null
+echo XDG_DATA_HOME=\$HOME/.local/share | sudo tee -a /etc/environment > /dev/null
 
 
 
@@ -22,7 +22,7 @@ sudo bash -c "echo XDG_DATA_HOME=\$HOME/.local/share >> /etc/environment"
 sudo pacman -S --noconfirm --needed zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search
 
 # Set ZSH dot directory
-sudo bash -c "echo 'export ZDOTDIR=\$HOME/.config/zsh' >> /etc/zsh/zshenv"
+echo "export ZDOTDIR=\$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv
 
 # Link ZSH files
 ln -sf `pwd`/zsh $HOME/.config/zsh
