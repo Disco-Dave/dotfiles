07daea28f87601e2827db5696f64c826ee7892a4
David Burkett <db.discodave@gmail.com>
2019-12-03 20:08:48 -0500

    Added sudo to the script commands

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@
 ###################################
 
 # Install zsh and xdg packages
-pacman -S --noconfirm --needed zsh xdg-utils xdg-user-dirs
+sudo pacman -S --noconfirm --needed zsh xdg-utils xdg-user-dirs
 
 # Ensure that the XDG env variables are set
 mkdir -p $HOME/.config $HOME/.cache $HOME/.local/share
-echo XDG_CONFIG_HOME=\$HOME/.config >> /etc/environment
-echo XDG_CACHE_HOME=\$HOME/.cache >> /etc/environment
-echo XDG_DATA_HOME=\$HOME/.local/share >> /etc/environment
+sudo echo XDG_CONFIG_HOME=\$HOME/.config >> /etc/environment
+sudo echo XDG_CACHE_HOME=\$HOME/.cache >> /etc/environment
+sudo echo XDG_DATA_HOME=\$HOME/.local/share >> /etc/environment
 
 # Set ZSH dot directory
-echo "export ZDOTDIR=\$HOME/.config/zsh" >> /etc/zsh/zshenv
+sudo echo "export ZDOTDIR=\$HOME/.config/zsh" >> /etc/zsh/zshenv
 
 # Link ZSH files
 ln -sf `pwd`/zsh $HOME/.config/zsh
