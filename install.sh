2deb1fa46dee68dcaad4aa1ceed22433aea5d30d
David Burkett <db.discodave@gmail.com>
2019-12-03 20:48:38 -0500

    Install oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 ###################################
-#       Setup shell and XDG       #
+#               XDG               #
 ###################################
-
-# Install zsh and xdg packages
-sudo pacman -S --noconfirm --needed zsh xdg-utils xdg-user-dirs
 
 # Ensure that the XDG env variables are set
 mkdir -p $HOME/.config $HOME/.cache $HOME/.local/share
@@ -13,8 +10,23 @@ sudo bash -c "echo XDG_CONFIG_HOME=\$HOME/.config >> /etc/environment"
 sudo bash -c "echo XDG_CACHE_HOME=\$HOME/.cache >> /etc/environment"
 sudo bash -c "echo XDG_DATA_HOME=\$HOME/.local/share >> /etc/environment"
 
+
+
+
+
+###################################
+#               SHELL             #
+###################################
+
+# Install zsh
+sudo pacman -S --noconfirm --needed zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search
+
 # Set ZSH dot directory
 sudo bash -c "echo 'export ZDOTDIR=\$HOME/.config/zsh' >> /etc/zsh/zshenv"
 
 # Link ZSH files
 ln -sf `pwd`/zsh $HOME/.config/zsh
+
+# Install oh-my-zsh
+export ZSH=$HOME/.config/oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,1 +1,2 @@
 export EDITOR='vim'
+export ZSH=$HOME/.config/oh-my-zsh
