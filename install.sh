a0ef383bcf22be017c3c18d29e69afc515b7f4f1
David Burkett <db.discodave@gmail.com>
2019-12-03 20:24:18 -0500

    Removed default shell command

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@
 # Install zsh and xdg packages
 sudo pacman -S --noconfirm --needed zsh xdg-utils xdg-user-dirs
 
-# Set ZSH as default shell
-chsh -s /bin/zsh
-
 # Ensure that the XDG env variables are set
 mkdir -p $HOME/.config $HOME/.cache $HOME/.local/share
 sudo bash -c "echo XDG_CONFIG_HOME=\$HOME/.config >> /etc/environment"
