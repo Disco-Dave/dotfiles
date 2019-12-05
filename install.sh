70d2966abf9907fdf6579ba0665a3bdef1a8e755
David Burkett <db.discodave@gmail.com>
2019-12-05 18:30:05 -0500

    asjdlkajdlk

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ mkdir -p $HOME/.config $HOME/.cache $HOME/.local/share
 xdg_vars=("export XDG_CONFIG_HOME=\$HOME/.config" 
           "export XDG_CACHE_HOME=\$HOME/.cache" 
           "export XDG_DATA_HOME=\$HOME/.localshare")
+if [ ! -f /etc/zsh/zshenv ]; then
+    sudo touch /etc/zsh/zshenv
+fi
 for var in $xdg_vars; do
-    echo "Adding " $var
     if grep -q $var /etc/zsh/zshenv; then
         echo $var | sudo tee -a /etc/zsh/zshenv > /dev/null
     fi
@@ -30,7 +32,6 @@ sudo pacman -S --noconfirm --needed zsh
 # Set ZSH dot directory
 $zdotdir="export ZDOTDIR=\$HOME/.config/zsh"
 if grep -q $zdotdir; then
-    echo "Adding " $zdotdir
     echo $zdotdir | sudo tee -a /etc/zsh/zshenv > /dev/null
 fi
 
@@ -42,7 +43,6 @@ touch $HOME/.local/share/zsh/history
 # Install oh-my-zsh
 export ZSH=$HOME/.config/oh-my-zsh
 if [ ! -d $ZSH ]; then
-    echo "Cloning oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/zsh-syntax-highlighting
     git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$ZSH/custom}/plugins/zsh-autosuggestions
@@ -71,7 +71,6 @@ mkdir -p $HOME/.local/share/vim/{undo,swap,backup}
 
 # Install vim-plug
 if [ ! -f ~/.local/share/vim/site/autoload/plug.vim ]; then
-    echo "Adding vim-plug"
     curl -fLo ~/.local/share/vim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
