0b0cd06510a3a9ccaa68226b34af53a9d4cf434a
David Burkett <db.discodave@gmail.com>
2020-05-18 23:38:00 +0000

    Fixed install script for zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install zsh packages
-sudo pacman -S --needed zsh zsh-autosuggestions zsh-completions \
+sudo pacman -S --needed --noconfirm zsh zsh-autosuggestions zsh-completions \
     zsh-history-substring-search zsh-syntax-highlighting
 
 # User XDG Base Directory for ZSH
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory#Support
-echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee -a /etc/zsh/zshenv > /dev/null
+echo "export ZDOTDIR=$HOME/.config/zsh" | sudo tee /etc/zsh/zshenv > /dev/null
 mkdir -p "$HOME/.config/zsh"
 mkdir -p "$HOME/.local/share/zsh"
 mkdir -p "$HOME/.cache/zsh"
@@ -16,4 +16,6 @@ ln -sf "`pwd`/zshenv" "$HOME/.config/zsh/.zshenv"
 ln -sf "`pwd`/zshrc" "$HOME/.config/zsh/.zshrc"
 
 # Set zsh as the default shell
-chsh -s /bin/zsh
+if [ "$SHELL" != "/bin/zsh" ]; then
+    chsh -s /bin/zsh
+fi
