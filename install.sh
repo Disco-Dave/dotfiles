70ade220ffd2663f372418fffbf825da3b3c8f32
David Burkett <db.discodave@gmail.com>
2020-05-18 20:18:45 -0400

    Made an install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ! -f "/bin/git" ]]; then
+    sudo pacman -S --noconfirm git
+fi
+
+if [[ ! -d "$HOME/.config/dotfiles" ]]; then
+    mkdir -p "$HOME/.config"
+    git clone https://github.com/Disco-Dave/dotfiles.git "$HOME/.config/dotfiles"
+
+    $(cd "$HOME/.config/dotfiles/git" && ./install.sh)
+    $(cd "$HOME/.config/dotfiles/zsh" && ./install.sh)
+fi
