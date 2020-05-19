db36f418461e33a15f86661dc3a552e9f58db6aa
David Burkett <david@davidburkett.xyz>
2020-05-19 17:49:36 -0400

    Fixed neovim script

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,12 +6,12 @@ set -e
 sudo pacman -S --needed --noconfirm neovim nodejs npm python python2 ruby xclip
 
 # Create npmrc
-if [[ ! -f "$XDG_CONFIG_HOME/npm/npmrc" ]]; then
-    mkdir -p "$XDG_CONFIG_HOME/npm"
-    echo "prefix=${XDG_DATA_HOME}/npm" >> "$XDG_CONFIG_HOME/npm"
-    echo "cache=${XDG_CACHE_HOME}/npm" >> "$XDG_CONFIG_HOME/npm"
-    echo "tmp=${XDG_RUNTIME_DIR}/npm" >> "$XDG_CONFIG_HOME/npm"
-    echo "init-module=${XDG_CONFIG_HOME}/npm/config/npm-init.js" >> "$XDG_CONFIG_HOME/npm"
+if [[ ! -f "$HOME/.config/npm/npmrc" ]]; then
+    mkdir -p "$HOME/.config/npm"
+    echo "prefix=${XDG_DATA_HOME}/npm" >> "$HOME/.config/npm"
+    echo "cache=${XDG_CACHE_HOME}/npm" >> "$HOME/.config/npm"
+    echo "tmp=${XDG_RUNTIME_DIR}/npm" >> "$HOME/.config/npm"
+    echo "init-module=${XDG_CONFIG_HOME}/npm/config/npm-init.js" >> "$HOME/.config/npm"
 fi
 
 # Create directories
