2a1ed0e6bc9e22708e4018ad3b1b37093cf7e8a9
Disco Dave <db0349@cs.ship.edu>
2020-05-19 17:20:57 -0400

    Update install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -13,7 +13,7 @@ mkdir -p "$HOME/.local/share/nvim/undo"
 mkdir -p "$HOME/.local/share/nvim/language-servers"
 
 # Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install all plugins via vim-plug
