49146b41d23984e1e3bcca6cb24c06b43ae3a729
David Burkett <db.discodave@gmail.com>
2020-05-19 21:46:25 -0400

    Fixed some paths maybe??

--- nvim/install.sh
+++ nvim/install.sh
@@ -8,11 +8,14 @@ sudo pacman -S --needed --noconfirm neovim nodejs npm python python2 ruby xclip
 # Create npmrc
 if [[ ! -f "$HOME/.config/npm/npmrc" ]]; then
     mkdir -p "$HOME/.config/npm"
-    echo "prefix=\${XDG_DATA_HOME}/npm" >> "$HOME/.config/npm/npmrc"
-    echo "cache=\${XDG_CACHE_HOME}/npm" >> "$HOME/.config/npm/npmrc"
-    echo "tmp=\${XDG_RUNTIME_DIR}/npm" >> "$HOME/.config/npm/npmrc"
-    echo "init-module=\${XDG_CONFIG_HOME}/npm/config/npm-init.js" >> "$HOME/.config/npm/npmrc"
+    echo "prefix=$HOME/.local/share/npm" >> "$HOME/.config/npm/npmrc"
+    echo "cache=$HOME/.cache/npm" >> "$HOME/.config/npm/npmrc"
+    echo "tmp=\$XDG_RUNTIME_DIR/npm" >> "$HOME/.config/npm/npmrc"
+    echo "init-module=$HOME/.config/npm/config/npm-init.js" >> "$HOME/.config/npm/npmrc"
 fi
+
+export NPM_CONFIG_USERCONFIG="$HOME/.config/npm/npmrc"
+npm install -g neovim
 
 # Create directories
 ln -sf "$(pwd)" "$HOME/.config/nvim"
