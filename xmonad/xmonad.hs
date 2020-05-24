36c548844a56caec2310c07b33d8f10077728907
David Burkett <david@davidburkett.xyz>
2020-05-23 23:13:57 -0400

    Added smart borders

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,6 +4,8 @@ import           XMonad
 
 import qualified XMonad.Util.SpawnOnce         as SpawnOnce
 
+import qualified XMonad.Layout.NoBorders       as NoBordersLayout
+
 import qualified XMonad.Hooks.DynamicLog       as DynamicLogHook
 import qualified XMonad.Hooks.EwmhDesktops     as EwmhHook
 
@@ -19,12 +21,14 @@ altKey = mod1Mask
 myStartupHook = do
   SpawnOnce.spawnOnce "mpv $XDG_CONFIG_HOME/dotfiles/assets/startup.mp3"
   SpawnOnce.spawnOnce "xmobar $XDG_CONFIG_HOME/xmobar/xmobar.config"
-  
+
+myLayoutHook = NoBordersLayout.smartBorders $ layoutHook def
 
 myConfig = def 
   { terminal = "alacritty"
   , modMask = mod1Mask 
   , startupHook = myStartupHook
+  , layoutHook = myLayoutHook
   }
 
 main :: IO ()
