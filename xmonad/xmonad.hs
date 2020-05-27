2633f2cd69170047443abea7e70c3adc5316fbe7
David Burkett <david@davidburkett.xyz>
2020-05-26 21:43:27 -0400

    Added some key bindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE RecordWildCards #-}
+
 import           Data.Function                  ( (&) )
 import           Data.Foldable                  ( for_ )
 
 import           XMonad
 
+import qualified XMonad.Util.EZConfig          as EZConfig
 import qualified XMonad.Util.Run               as Run
 import qualified XMonad.Util.SpawnOnce         as SpawnOnce
 
@@ -12,11 +15,32 @@ import qualified XMonad.Hooks.DynamicLog       as DynamicLogHook
 import qualified XMonad.Hooks.EwmhDesktops     as EwmhHook
 import qualified XMonad.Hooks.ManageDocks      as ManageDocksHook
 
+import qualified Graphics.X11                  as X11
+import qualified Graphics.X11.ExtraTypes       as X11
 
 (superKey, altKey) = (mod4Mask, mod1Mask)
 
+additionalKeys XConfig{..} = 
+  [ -- Volume keys
+    ((X11.noModMask, X11.xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 5%+")
+  , ((X11.noModMask, X11.xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 5%-")
+  , ((X11.shiftMask, X11.xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 1%+")
+  , ((X11.shiftMask, X11.xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 1%-")
+  , ((X11.noModMask, X11.xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle")
+  , ((superKey, xK_v), spawn "pavucontrol")
+
+  -- Web browser
+  , ((superKey, xK_w), spawn "firefox")
+
+  -- Screen shooter
+  , ((X11.noModMask, xK_Print), spawn "xfce4-screenshooter")
+  , ((X11.controlMask, xK_Print), spawn "xfce4-screenshooter -r")
+  , ((X11.shiftMask, xK_Print), spawn "xfce4-screenshooter -f")
+  ]
+
 myStartupHook = do
   SpawnOnce.spawnOnce "mpv $XDG_CONFIG_HOME/dotfiles/assets/startup.mp3"
+  SpawnOnce.spawnOnce "stalonetray -c $XDG_CONFIG_HOME/stalonetray/stalonetrayrc"
   SpawnOnce.spawnOnce "nm-applet"
   SpawnOnce.spawnOnce "xfce4-power-manager"
   SpawnOnce.spawnOnce "pasystray"
@@ -27,22 +51,26 @@ myLayoutHook = layoutHook def
   & NoBordersLayout.smartBorders
   & ManageDocksHook.avoidStruts
 
-makeConfig handles = def
-    { terminal    = "alacritty"
-    , modMask     = mod1Mask
-    , startupHook = myStartupHook
-    , layoutHook  = myLayoutHook
-    , logHook     = DynamicLogHook.dynamicLogWithPP $ DynamicLogHook.xmobarPP
-      { DynamicLogHook.ppOutput = \p -> for_ handles $ \h -> Run.hPutStrLn h p
+makeConfig handles = 
+  let 
+    baseConf = def
+      { terminal    = "alacritty"
+      , modMask     = mod1Mask
+      , startupHook = myStartupHook
+      , layoutHook  = myLayoutHook
+      , logHook     = DynamicLogHook.dynamicLogWithPP $ DynamicLogHook.xmobarPP
+        { DynamicLogHook.ppOutput = \p -> for_ handles $ \h -> Run.hPutStrLn h p
+        }
       }
-    }
-  & EwmhHook.ewmh
-  & EwmhHook.ewmhFullscreen
-  & ManageDocksHook.docks
+  in 
+    baseConf
+    & EwmhHook.ewmh
+    & EwmhHook.ewmhFullscreen
+    & ManageDocksHook.docks
+    & flip EZConfig.additionalKeys (additionalKeys baseConf)
 
 main :: IO ()
 main = do
   xmobar <- Run.spawnPipe "xmobar-primary"
-  _ <- Run.spawnPipe "stalonetray -c $XDG_CONFIG_HOME/stalonetray/stalonetrayrc"
   let config = makeConfig [xmobar]
   xmonad config
