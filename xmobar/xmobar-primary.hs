dbec7d907a73dc2e1be4ad1e71335bc95b86f79f
David Burkett <david@davidburkett.xyz>
2020-06-06 13:33:08 -0400

    Added some simple mpd information that shows up when a song isn't stopped

--- xmobar/xmobar-primary.hs
+++ xmobar/xmobar-primary.hs
@@ -1,31 +1,35 @@
-import           Xmobar
+import Xmobar
 
 config :: Config
-config = defaultConfig
-  { font             = "xft:FreeSans-12"
-  , additionalFonts  = []
-  , border           = FullB
-  , borderColor      = "#3B4252"
-  , bgColor          = "#3B4252"
-  , fgColor          = "#ECEFF4"
-  , alpha            = 255
-  , position         = Top
-  , textOffset       = -1
-  , iconOffset       = -1
-  , lowerOnStart     = True
-  , pickBroadest     = False
-  , persistent       = False
-  , hideOnStart      = False
-  , iconRoot         = "."
-  , allDesktops      = True
-  , overrideRedirect = True
-  , commands         = [ Run $ Date "%a %b %_d %Y %I:%M:%S %p" "date" 10
-                       , Run UnsafeStdinReader
-                       ]
-  , sepChar          = "%"
-  , alignSep         = "}{"
-  , template         = "%UnsafeStdinReader% } %date% { "
-  }
+config =
+  defaultConfig
+    { font = "xft:FreeSans-12",
+      additionalFonts = [],
+      border = FullB,
+      borderColor = "#3B4252",
+      bgColor = "#3B4252",
+      fgColor = "#ECEFF4",
+      alpha = 255,
+      position = OnScreen 0 Top,
+      textOffset = -1,
+      iconOffset = -1,
+      lowerOnStart = True,
+      pickBroadest = False,
+      persistent = False,
+      hideOnStart = False,
+      iconRoot = ".",
+      allDesktops = True,
+      overrideRedirect = True,
+      commands =
+        [ Run $ Date "%a %b %_d %Y %I:%M:%S %p" "date" 10,
+          Run UnsafeStdinReader,
+          Run $ Com "mpc" ["current", "-f", "%title% by %artist%"] "mpd" 10,
+          Run $ Com "bash" ["/home/disco/.config/xmobar/padding-icon.sh", "stalonetray"] "tray" 10
+        ],
+      sepChar = "%",
+      alignSep = "}{|",
+      template = "%UnsafeStdinReader% } %date% { %mpd% %tray%"
+    }
 
 main :: IO ()
 main = xmobar config
