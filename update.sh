2311c6615d1b357c5b5cce107513b4eb28cbd25e
David Burkett <david@davidburkett.xyz>
2020-06-07 00:05:03 -0400

    Editted my update script

--- update.sh
+++ update.sh
@@ -9,3 +9,4 @@ npm -g update neovim
 stack upgrade
 (cd $HOME/.local/share/zsh/plugins/zsh-git-prompt; git pull; stack install)
 nvim +PlugUpdate +qall
+(cd $HOME/.config/alacritty/gen-alacritty; stack install; gen-alacritty)
