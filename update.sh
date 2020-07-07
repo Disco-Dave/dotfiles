a7fb6601c7798c73a4476c6d684c905ea01ae0ca
David Burkett <david@davidburkett.xyz>
2020-07-06 22:46:21 -0400

    Update rust up

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ sudo pacman -Syu --noconfirm
 yay --aur -Syu --noconfirm
 npm -g update neovim
 stack upgrade
+rustup update
 (cd $HOME/.local/share/zsh/plugins/zsh-git-prompt; git pull; stack install)
 nvim +PlugUpdate +qall
 (cd $HOME/.config/alacritty/gen-alacritty; stack install; gen-alacritty)
