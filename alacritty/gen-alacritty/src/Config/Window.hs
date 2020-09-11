979c6def412526c833e436ce49fc65a419293cf2
David Burkett <david@davidburkett.xyz>
2020-09-11 15:51:42 -0400

    Fixed gen-alacritty

--- alacritty/gen-alacritty/src/Config/Window.hs
+++ alacritty/gen-alacritty/src/Config/Window.hs
@@ -1,11 +1,14 @@
 module Config.Window where
 
 import qualified Data.Aeson as Aeson
-import GHC.Generics (Generic)
 
 newtype Window = Window
   { dynamicTitle :: Bool
   }
-  deriving (Show, Eq, Generic)
+  deriving (Show, Eq)
 
-instance Aeson.ToJSON Window
+instance Aeson.ToJSON Window where
+  toJSON Window {..} =
+    Aeson.object ["dynamic_title" Aeson..= dynamicTitle]
+  toEncoding Window {..} =
+    Aeson.pairs $ "dynamic_title" Aeson..= dynamicTitle
