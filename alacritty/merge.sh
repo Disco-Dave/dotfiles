2af4642ebab3378bca63ea70891290bc8e559d54
David Burkett <david@davidburkett.xyz>
2021-05-27 22:50:03 -0400

    Replaced the gen-alacritty program with a script using yaml-merge

---
+++ alacritty/merge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+path_to_yaml_merge=$(which yaml-merge)
+
+if [ ! -x "$path_to_yaml_merge" ]; then
+    >&2 echo "Downloading and installing yaml-merge"
+
+    (
+        cd /tmp
+
+        if [ ! -d "yaml-merge" ]; then
+            git clone https://github.com/Disco-Dave/yaml-merge 
+        fi
+
+        cd yaml-merge
+        stack install
+    )
+fi
+
+host_name_config="$(hostname).yml"
+
+if [ -f "$host_name_config" ]; then
+    >&2 echo "Overlaying host specific settings"
+    yaml-merge "$host_name_config" base.yml > alacritty.yml
+fi
