934044a3cf6ee8661523e187a73304b79e57eacc
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-01-08 19:39:30 -0500

    Added rust, and package managers

---
+++ install/04-setup-rust.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "-- Setup Rust --"
+
+source "$_DOTFILES_HOME/zsh/zshenv"
+set -e
+
+sudo pacman -S --noconfirm --needed rustup openssl pkgconf
+
+rustup toolchain install stable
+
+cargo install cargo-watch cargo-tarpaulin cargo-edit cargo-update
---
+++ install/05-setup-package-managers.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+echo "-- Setup Package Managers --"
+
+source "$_DOTFILES_HOME/zsh/zshenv"
+set -e
+
+sudo pacman -S --noconfirm --needed reflector fakeroot
+
+sudo ln -sf "$_DOTFILES_HOME/pacman/reflector.conf" /etc/xdg/reflector/reflector.conf
+sudo ln -sf "$_DOTFILES_HOME/pacman/pacman.conf" /etc/pacman.conf
+
+if [ -ne "$ENVIRONMENT" "sandbox" ]; then
+  sudo systemctl enable --now reflector.timer
+fi
+
+cargo install topgrade
+
+if [[ ! -f "/usr/bin/paru" ]]; then
+  work_dir=$(mktemp -d) 
+
+  trap "rm -rf $work_dir" EXIT
+
+  cd $work_dir
+
+  curl -o paru-bin.tar.gz https://aur.archlinux.org/cgit/aur.git/snapshot/paru-bin.tar.gz
+  tar xf paru-bin.tar.gz
+
+  cd paru-bin
+  makepkg -si --noconfirm
+fi
