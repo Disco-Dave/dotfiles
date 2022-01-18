f826c0f8d1aeb66a72a2c6caec54e53d82cd220b
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-01-17 21:24:05 -0500

    Made some major edits

--- install/09-setup-desktop.sh
+++ install/09-setup-desktop.sh
@@ -5,22 +5,81 @@ echo "-- Setup Desktop --"
 source "$_DOTFILES_HOME/zsh/zshenv"
 set -e
 
-# TODO fonts
-# TODO icons
-# TODO cursors
-# TODO themes
+if [ "$ENVIRONMENT" != "sandbox" ]; then
+  sudo pacman -S --needed --noconfirm \
+    alacritty \
+    arc-gtk-theme \
+    arc-icon-theme \
+    atril \
+    blueman \
+    bluez \
+    bluez-utils \
+    dmenu \
+    engrampa \
+    feh \
+    firefox \
+    gvfs \
+    htop \
+    libx11 \
+    libxft \
+    libxinerama \
+    libxrandr \
+    libxss \
+    network-manager-applet \
+    p7zip \
+    pass \
+    pasystray \
+    pavucontrol \
+    picom \
+    pkgconf \
+    pulseaudio \
+    pulseaudio-alsa \
+    pulseaudio-bluetooth \
+    pulsemixer \
+    redshift \
+    stalonetray \
+    thunar \
+    ttf-hack \
+    udisks2 \
+    xclip \
+    xcursor-vanilla-dmz \
+    xdotool \
+    xfce4-notifyd \
+    xfce4-power-manager \
+    xfce4-screenshooter \
+    xorg
 
-# TODO Audio including bluetooth
+  # Enable bluetooth
+  sudo systemctl enable --now bluetooth.service
 
-# xmonad
-sudo pacman -S --needed --noconfirm git xorg libx11 libxft libxinerama libxrandr libxss pkgconf
-ln -sf "$_DOTFILES_HOME/xmonad" "$XDG_CONFIG_HOME/xmonad"
-(
-  cd "$XDG_CONFIG_HOME/xmonad"
-  cabal install
-  xmonad --recompile
-)
+  # icons
+  ln -sf "$_DOTFILES_HOME/icons" "$HOME/.icons"
 
-# TODO picom
-# TODO stalonetray
-# TODO alacritty
+  # themes
+  ln -sf "$_DOTFILES_HOME/gtk-2.0" "$XDG_CONFIG_HOME/gtk-2.0"
+  ln -sf "$_DOTFILES_HOME/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
+
+  # X11
+  ln -sf "$_DOTFILES_HOME/X11" "$XDG_CONFIG_HOME/X11"
+
+  # xmonad
+  ln -sf "$_DOTFILES_HOME/xmonad" "$XDG_CONFIG_HOME/xmonad"
+  (
+    cd "$XDG_CONFIG_HOME/xmonad"
+    cabal install
+    xmonad --recompile
+  )
+
+  # picom
+  ln -sf "$_DOTFILES_HOME/picom" "$XDG_CONFIG_HOME/picom"
+
+  # stalonetray
+  ln -sf "$_DOTFILES_HOME/stalonetray" "$XDG_CONFIG_HOME/stalonetray"
+
+  # alacritty
+  ln -sf "$_DOTFILES_HOME/alacritty" "$XDG_CONFIG_HOME/alacritty"
+  (
+    cd "$XDG_CONFIG_HOME/alacritty"
+    ./merge.sh
+  )
+fi
