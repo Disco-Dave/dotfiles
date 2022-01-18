ec1adad0dec0515f1a404a7c76b37b0c04c37018
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-01-17 20:55:15 -0500

    foo

--- install/09-setup-desktop.sh
+++ install/09-setup-desktop.sh
@@ -5,20 +5,22 @@ echo "-- Setup Desktop --"
 source "$_DOTFILES_HOME/zsh/zshenv"
 set -e
 
+# TODO fonts
+# TODO icons
+# TODO cursors
+# TODO themes
+
 # TODO Audio including bluetooth
 
 # xmonad
 sudo pacman -S --needed --noconfirm git xorg libx11 libxft libxinerama libxrandr libxss pkgconf
-
 ln -sf "$_DOTFILES_HOME/xmonad" "$XDG_CONFIG_HOME/xmonad"
 (
   cd "$XDG_CONFIG_HOME/xmonad"
   cabal install
+  xmonad --recompile
 )
 
-# TODO xmobar
 # TODO picom
 # TODO stalonetray
 # TODO alacritty
-# TODO gtk
-# TODO icons
