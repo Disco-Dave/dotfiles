87493ae6b3ab8e803c06bfe6a6f1cfb7404307a3
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-01-19 18:51:48 -0500

    Removed unused functions

--- xmonad/app/MyXmobar.hs
+++ xmonad/app/MyXmobar.hs
@@ -3,17 +3,12 @@ module MyXmobar (
   left,
   right,
   withXmobar,
-  spawnLeft,
-  spawnRight,
 ) where
 
 import qualified Control.Concurrent.Async as Async
 import qualified Control.Concurrent.STM as STM
 import Data.Foldable (for_)
 import Data.Traversable (for)
-import System.IO (Handle)
-import System.Posix (forkProcess)
-import System.Process (createPipe)
 import qualified Xmobar
 
 newtype XmobarSettings = XmobarSettings
@@ -96,75 +91,3 @@ withXmobar settings useXmobar = do
 
   Async.withAsync xmobars $ \x ->
     useXmobar send <* Async.cancel x
-
-spawn :: (Handle -> Xmobar.Config) -> IO Handle
-spawn config = do
-  (readHandle, writeHandle) <- createPipe
-
-  _ <- forkProcess . Xmobar.xmobar $ config readHandle
-  pure writeHandle
-
-spawnLeft :: IO Handle
-spawnLeft =
-  spawn $ \readHandle ->
-    Xmobar.defaultConfig
-      { Xmobar.font = "xft:FreeSans-12"
-      , Xmobar.additionalFonts = []
-      , Xmobar.border = Xmobar.FullB
-      , Xmobar.borderColor = "#3B4252"
-      , Xmobar.bgColor = "#3B4252"
-      , Xmobar.fgColor = "#ECEFF4"
-      , Xmobar.alpha = 255
-      , Xmobar.position = Xmobar.OnScreen 0 Xmobar.Top
-      , Xmobar.textOffset = -1
-      , Xmobar.iconOffset = -1
-      , Xmobar.lowerOnStart = True
-      , Xmobar.pickBroadest = False
-      , Xmobar.persistent = False
-      , Xmobar.hideOnStart = False
-      , Xmobar.iconRoot = "."
-      , Xmobar.allDesktops = True
-      , Xmobar.overrideRedirect = True
-      , Xmobar.commands =
-          [ Xmobar.Run $ Xmobar.Date "%a %b %_d %Y %I:%M:%S %p" "date" 10
-          , Xmobar.Run $ Xmobar.HandleReader readHandle "reader"
-          --, Xmobar.Run $ Xmobar.ComX "mpc" ["current", "-f", "%title% by %artist%"] "" "mpd" 10
-          --, Xmobar.Run $ Xmobar.Com "bash" [paddingScriptPath, "stalonetray"] "tray" 10
-          ]
-      , Xmobar.sepChar = "%"
-      , Xmobar.alignSep = "}{"
-      , Xmobar.template = "%reader% } %date% { "
-      }
-
-spawnRight :: IO Handle
-spawnRight =
-  spawn $ \readHandle ->
-    Xmobar.defaultConfig
-      { Xmobar.font = "xft:FreeSans-12"
-      , Xmobar.additionalFonts = []
-      , Xmobar.border = Xmobar.FullB
-      , Xmobar.borderColor = "#3B4252"
-      , Xmobar.bgColor = "#3B4252"
-      , Xmobar.fgColor = "#ECEFF4"
-      , Xmobar.alpha = 255
-      , Xmobar.position = Xmobar.OnScreen 1 Xmobar.Top
-      , Xmobar.textOffset = -1
-      , Xmobar.iconOffset = -1
-      , Xmobar.lowerOnStart = True
-      , Xmobar.pickBroadest = False
-      , Xmobar.persistent = False
-      , Xmobar.hideOnStart = False
-      , Xmobar.iconRoot = "."
-      , Xmobar.allDesktops = True
-      , Xmobar.overrideRedirect = True
-      , Xmobar.commands =
-          [ Xmobar.Run $ Xmobar.HandleReader readHandle "reader"
-          , Xmobar.Run $ Xmobar.ComX "curl" ["-s", "-G", "-d", "format=%C,%20%t", "wttr.in/17070"] "" "weather" 6000
-          , Xmobar.Run $ Xmobar.Memory ["-t", "Mem: <usedratio>%"] 10
-          , Xmobar.Run $ Xmobar.Swap [] 10
-          , Xmobar.Run $ Xmobar.Cpu [] 10
-          ]
-      , Xmobar.sepChar = "%"
-      , Xmobar.alignSep = "}{"
-      , Xmobar.template = "%reader% } %weather% { %cpu% | %memory% * %swap%"
-      }
