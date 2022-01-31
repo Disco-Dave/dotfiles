b9bff8065ec823e73d87d657b57398f937aa82c2
David Burkett <13578196+Disco-Dave@users.noreply.github.com>
2022-01-30 21:24:34 -0500

    more stuff

--- migrations/1643425749__setup_desktop.sh
+++ migrations/1643425749__setup_desktop.sh
@@ -67,25 +67,25 @@ if [ "$ENVIRONMENT" != "sandbox" ]; then
   systemctl --user enable --now pulseaudio
 
   # icons
-  if [[ ! -e "$_DOTFILES_HOME/icons" ]]; then
+  if [[ ! -e "$HOME/.icons" ]]; then
     ln -sf "$_DOTFILES_HOME/icons" "$HOME/.icons"
   fi
 
   # themes
-  if [[ ! -e "$_DOTFILES_HOME/gtk-2.0" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/gtk-2.0" ]]; then
     ln -sf "$_DOTFILES_HOME/gtk-2.0" "$XDG_CONFIG_HOME/gtk-2.0"
   fi
-  if [[ ! -e "$_DOTFILES_HOME/gtk-3.0" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/gtk-3.0" ]]; then
     ln -sf "$_DOTFILES_HOME/gtk-3.0" "$XDG_CONFIG_HOME/gtk-3.0"
   fi
 
   # X11
-  if [[ ! -e "$_DOTFILES_HOME/X11" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/X11" ]]; then
     ln -sf "$_DOTFILES_HOME/X11" "$XDG_CONFIG_HOME/X11"
   fi
 
   # xmobar
-  if [[ ! -e "$_DOTFILES_HOME/xmobar" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/xmobar" ]]; then
     ln -sf "$_DOTFILES_HOME/xmobar" "$XDG_CONFIG_HOME/xmobar"
   fi
   (
@@ -94,7 +94,7 @@ if [ "$ENVIRONMENT" != "sandbox" ]; then
   )
 
   # xmonad
-  if [[ ! -e "$_DOTFILES_HOME/xmonad" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/xmonad" ]]; then
     ln -sf "$_DOTFILES_HOME/xmonad" "$XDG_CONFIG_HOME/xmonad"
   fi
   (
@@ -104,17 +104,17 @@ if [ "$ENVIRONMENT" != "sandbox" ]; then
   )
 
   # picom
-  if [[ ! -e "$_DOTFILES_HOME/picom/picom.conf" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/picom/picom.conf" ]]; then
     ln -sf "$_DOTFILES_HOME/picom/picom.conf" "$XDG_CONFIG_HOME/picom/picom.conf"
   fi
 
   # stalonetray
-  if [[ ! -e "$_DOTFILES_HOME/stalonetray" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/stalonetray" ]]; then
     ln -sf "$_DOTFILES_HOME/stalonetray" "$XDG_CONFIG_HOME/stalonetray"
   fi
 
   # alacritty
-  if [[ ! -e "$_DOTFILES_HOME/alacritty" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/alacritty" ]]; then
     ln -sf "$_DOTFILES_HOME/alacritty" "$XDG_CONFIG_HOME/alacritty"
   fi
   (
@@ -123,13 +123,12 @@ if [ "$ENVIRONMENT" != "sandbox" ]; then
   )
 
   # xrandr
-  if [[ ! -e "$_DOTFILES_HOME/xrandr" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/xrandr" ]]; then
     ln -sf "$_DOTFILES_HOME/xrandr" "$XDG_CONFIG_HOME/xrandr"
   fi
 
-  # TODO NEEDS FIXED!
   # mpd
-  if [[ ! -e "$_DOTFILES_HOME/mpd" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/mpd" ]]; then
     ln -sf "$_DOTFILES_HOME/mpd" "$XDG_CONFIG_HOME/mpd"
   fi
   mkdir -p "$XDG_DATA_HOME/mpd/playlists"
@@ -138,7 +137,7 @@ if [ "$ENVIRONMENT" != "sandbox" ]; then
   mpc update
 
   # ncmpcpp
-  if [[ ! -e "$_DOTFILES_HOME/ncmpcpp" ]]; then
+  if [[ ! -e "$XDG_CONFIG_HOME/ncmpcpp" ]]; then
     ln -sf "$_DOTFILES_HOME/ncmpcpp" "$XDG_CONFIG_HOME/ncmpcpp"
   fi
 fi
