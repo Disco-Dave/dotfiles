d244bbea113070d387bd71b0c5fe797614482fa7
David Burkett <david.burkett@motorefi.com>
2022-07-12 07:07:17 -0400

    removed a commented out line

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -5,7 +5,7 @@ return function()
   end
 
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-  local formatting = null_ls.builtins.formatting
+  -- local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
   local diagnostics = null_ls.builtins.diagnostics
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
@@ -13,7 +13,6 @@ return function()
 
   null_ls.setup({
     sources = {
-      -- formatting.fourmolu, TODO
       diagnostics.shellcheck,
       code_actions.shellcheck,
     },
