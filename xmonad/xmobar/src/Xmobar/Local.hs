653563819824b2386e18498ff51c682e815efdf0
disco-dave <1124825-Disco-Dave@users.noreply.gitlab.com>
2022-11-16 20:39:06 -0500

    fixed xmobar for desktop

--- xmonad/xmobar/src/Xmobar/Local.hs
+++ xmonad/xmobar/src/Xmobar/Local.hs
@@ -3,7 +3,6 @@ module Xmobar.Local (start) where
 import Shared.Environment (Environment (..), getEnvironment)
 import Shared.FilePaths qualified as FilePaths
 import Shared.Hostname qualified as Hostname
-import Shared.Theme (Theme)
 import Shared.Theme qualified as Theme
 import Shared.Theme.Color qualified as Color
 import Shared.Theme.Font qualified as Font
@@ -12,21 +11,35 @@ import System.FilePath qualified as FilePath
 import Xmobar qualified
 
 
-defaultConfig :: Theme -> Xmobar.Config
-defaultConfig theme =
-  Xmobar.defaultConfig
-    { Xmobar.font = Font.toPangoString theme.font
-    , Xmobar.borderColor = Color.toHashString theme.xmobar.border
-    , Xmobar.bgColor = Color.toHashString theme.xmobar.background
-    , Xmobar.fgColor = Color.toHashString theme.xmobar.foreground
-    , Xmobar.lowerOnStart = True
-    , Xmobar.pickBroadest = False
-    , Xmobar.persistent = False
-    , Xmobar.hideOnStart = False
-    , Xmobar.textOutputFormat = Xmobar.Pango
-    , Xmobar.allDesktops = True
-    , Xmobar.overrideRedirect = True
-    }
+defaultConfig :: Environment -> Int -> Xmobar.Config
+defaultConfig env screenId =
+  let position =
+        case env.hostname of
+          Hostname.Laptop ->
+            Xmobar.OnScreen screenId (Xmobar.TopH 22)
+          _ ->
+            Xmobar.OnScreen screenId (Xmobar.TopH 30)
+
+      font =
+        case env.hostname of
+          Hostname.Laptop ->
+            Font.toPangoString env.theme.font
+          _ ->
+            Font.toPangoString (env.theme.font{Font.size = 17})
+   in Xmobar.defaultConfig
+        { Xmobar.font = font
+        , Xmobar.position = position
+        , Xmobar.borderColor = Color.toHashString env.theme.xmobar.border
+        , Xmobar.bgColor = Color.toHashString env.theme.xmobar.background
+        , Xmobar.fgColor = Color.toHashString env.theme.xmobar.foreground
+        , Xmobar.lowerOnStart = True
+        , Xmobar.pickBroadest = False
+        , Xmobar.persistent = False
+        , Xmobar.hideOnStart = False
+        , Xmobar.textOutputFormat = Xmobar.Pango
+        , Xmobar.allDesktops = True
+        , Xmobar.overrideRedirect = True
+        }
 
 
 primary :: Environment -> Xmobar.Config
@@ -38,11 +51,12 @@ primary env =
           , "xmobar"
           , "padding-icon.sh"
           ]
+
+      config = defaultConfig env 0
    in case env.hostname of
         Hostname.Work ->
-          (defaultConfig env.theme)
-            { Xmobar.position = Xmobar.OnScreen 0 Xmobar.Top
-            , Xmobar.commands =
+          config
+            { Xmobar.commands =
                 [ Xmobar.Run $ Xmobar.UnsafeXPropertyLog "_XMONAD_LOG_1"
                 , Xmobar.Run $ Xmobar.Date "%a %b %_d %Y %I:%M:%S %p" "date" 10
                 , Xmobar.Run $ Xmobar.Com "bash" [paddingIconScript, "panel"] "tray" 10
@@ -50,9 +64,8 @@ primary env =
             , Xmobar.template = "%_XMONAD_LOG_1% } %date% { %tray%"
             }
         _ ->
-          (defaultConfig env.theme)
-            { Xmobar.position = Xmobar.OnScreen 0 (Xmobar.TopH 25)
-            , Xmobar.commands =
+          config
+            { Xmobar.commands =
                 [ Xmobar.Run $ Xmobar.UnsafeXPropertyLog "_XMONAD_LOG_1"
                 , Xmobar.Run $ Xmobar.Date "%a %b %_d %Y %I:%M:%S %p" "date" 10
                 , Xmobar.Run $ Xmobar.ComX "mpc" ["current", "-f", "%title% by %artist%"] "" "mpd" 10
@@ -64,9 +77,8 @@ primary env =
 
 secondary :: Environment -> Xmobar.Config
 secondary env =
-  (defaultConfig env.theme)
-    { Xmobar.position = Xmobar.OnScreen 2 Xmobar.Top
-    , Xmobar.commands =
+  (defaultConfig env 2)
+    { Xmobar.commands =
         [ Xmobar.Run $ Xmobar.UnsafeXPropertyLog "_XMONAD_LOG_2"
         ]
     , Xmobar.template = "%_XMONAD_LOG_2% } { "
@@ -75,9 +87,8 @@ secondary env =
 
 tertiary :: Environment -> Xmobar.Config
 tertiary env =
-  (defaultConfig env.theme)
-    { Xmobar.position = Xmobar.OnScreen 1 Xmobar.Top
-    , Xmobar.commands =
+  (defaultConfig env 1)
+    { Xmobar.commands =
         [ Xmobar.Run $ Xmobar.UnsafeXPropertyLog "_XMONAD_LOG_3"
         , Xmobar.Run $ Xmobar.Memory ["-t", "Mem: <usedratio>%"] 10
         , Xmobar.Run $ Xmobar.Swap [] 10
